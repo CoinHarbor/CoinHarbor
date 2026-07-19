9e3f11f3826cd53a2a2dbdbed9cc010a594350db
CoinHarbor <coinharbor6@gmail.com>
2026-07-19 11:23:10 +0100

    Add files via upload

---
+++ navigation.ts
@@ -0,0 +1,27 @@
+export const navigation = [
+  {
+    name: "Home",
+    href: "#",
+    id: "home",
+  },
+  {
+    name: "Services",
+    href: "#services",
+    id: "services",
+  },
+  {
+    name: "Products",
+    href: "#products",
+    id: "products",
+  },
+  {
+    name: "Security",
+    href: "#security",
+    id: "security",
+  },
+  {
+    name: "FAQ",
+    href: "#faq",
+    id: "faq",
+  },
+];
